00ca9e5ba234f8401672c3c0e9cd56afdef21819
shlok-2003 <shlokprajapati2003@gmail.com>
2024-03-16 14:25:53 +0530

    Added icons

---
+++ client/src/icons/index.ts
@@ -0,0 +1,112 @@
+import { IconType } from "react-icons";
+
+import {
+    FaStar,
+    FaChevronUp,
+    FaLinkedin,
+    FaGithub,
+    FaTwitter,
+    FaInstagram,
+    FaFacebook,
+    FaHeart,
+    FaFilePdf,
+    FaRegEnvelope,
+} from "react-icons/fa";
+
+import { FaDollarSign, FaSquareInstagram } from "react-icons/fa6";
+
+import {
+    VscVm,
+    VscAdd,
+    VscBook,
+    VscCallOutgoing,
+    VscAccount,
+    VscDashboard,
+    VscMortarBoard,
+    VscHistory,
+} from "react-icons/vsc";
+
+import { FiUsers, FiArrowRight } from "react-icons/fi";
+
+import {
+    HiOutlineMenuAlt1,
+    HiOutlineViewGrid,
+    HiOutlineCube,
+    HiOutlineShoppingCart,
+    HiOutlineUsers,
+    HiOutlineDocumentText,
+    HiOutlineAnnotation,
+    HiOutlineQuestionMarkCircle,
+    HiOutlineCog,
+    HiOutlineLogout,
+} from "react-icons/hi";
+
+import { AiOutlineEye, AiOutlineEyeInvisible, AiFillInstagram } from "react-icons/ai";
+
+import { BiWorld, BiArrowBack } from "react-icons/bi";
+
+import { HiChatBubbleLeftRight } from "react-icons/hi2";
+
+import { IoCall, IoBagHandle, IoPieChart, IoPeople } from "react-icons/io5";
+
+import { BsGraphUpArrow } from "react-icons/bs";
+
+import { ImTree } from "react-icons/im";
+
+import { IoMdMenu } from "react-icons/io";
+
+import { RxDashboard } from "react-icons/rx";
+
+import { MdInsertLink } from "react-icons/md";
+
+export {
+    FiUsers,
+    FaStar,
+    FaHeart,
+    FaChevronUp,
+    FiArrowRight,
+    FaTwitter,
+    FaGithub,
+    FaInstagram,
+    FaFacebook,
+    FaFilePdf,
+    FaRegEnvelope,
+    FaDollarSign,
+    FaSquareInstagram,
+    FaLinkedin,
+    HiOutlineMenuAlt1,
+    HiOutlineViewGrid,
+    HiOutlineCube,
+    HiOutlineShoppingCart,
+    HiOutlineUsers,
+    HiOutlineDocumentText,
+    HiOutlineAnnotation,
+    HiOutlineQuestionMarkCircle,
+    HiOutlineCog,
+    HiOutlineLogout,
+    HiChatBubbleLeftRight,
+    AiOutlineEye,
+    AiOutlineEyeInvisible,
+    AiFillInstagram,
+    BiWorld,
+    BiArrowBack,
+    IoCall,
+    IoBagHandle,
+    IoPieChart,
+    IoPeople,
+    ImTree,
+    VscVm,
+    VscAdd,
+    VscBook,
+    VscHistory,
+    VscAccount,
+    VscDashboard,
+    VscMortarBoard,
+    VscCallOutgoing,
+    BsGraphUpArrow,
+    IoMdMenu,
+    RxDashboard,
+    MdInsertLink,
+};
+
+export type { IconType };
