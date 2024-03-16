c834d8db2bef9dadae024c23c2b455e4890ee9ff
shlok-2003 <shlokprajapati2003@gmail.com>
2024-03-16 14:49:38 +0530

    Added typescript types

---
+++ client/src/types/index.ts
@@ -0,0 +1,26 @@
+import { IconType } from 'react-icons'
+
+export interface NavItem {
+    title: string;
+    link?: string;
+    disabled?: boolean;
+    
+    external?: boolean;
+    
+    icon: IconType;
+    iconColor?: string;
+    
+    label?: string;
+    description?: string;
+}
+
+export interface NavGroupWithChildren extends NavItem {
+    items: NavGroupWithChildren[];
+}
+
+export interface NavGroupWithOptionalChildren extends NavItem {
+    items?: NavGroupWithChildren[];
+}
+
+export type MainNavbar = NavGroupWithOptionalChildren;
+export type Sidebar = NavGroupWithChildren;
---
+++ client/src/types/main.ts
@@ -0,0 +1,11 @@
+export interface User {
+    name: string;
+    username: string;
+    email: string;
+    profileImg: string;
+    bio: string;
+
+    instagram: string;
+    twitter: string;
+    github: string;
+}
