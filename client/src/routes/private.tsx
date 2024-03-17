56d18f1788ce7868e0fe3cd9795cae600a498bb8
shlok-2003 <shlokprajapati2003@gmail.com>
2024-03-17 11:54:53 +0530

    added 12

--- client/src/routes/private.tsx
+++ client/src/routes/private.tsx
@@ -6,40 +6,55 @@ import { toast } from "sonner";
 const PrivateRoute: React.FC<{ children?: React.ReactNode }> = ({
     children,
 }) => {
-    // const location = useLocation();
-    // const userString = localStorage.getItem("User");
+    const location = useLocation();
+    const userString = localStorage.getItem("User");
 
-    // console.log("User", userString);
+    console.log("User", userString);
 
-    // if (!userString) {
-    //     toast.error("Please login to access this page", {
-    //         position: "bottom-right",
-    //     });
+    if (!userString) {
+        toast.error("Please login to access this page", {
+            position: "bottom-right",
+        });
 
-    //     console.log("User not found");
-    //     setTimeout(() => {}, 2000);
-    //     return <Navigate to="/login" />;
-    // }
+        console.log("User not found");
+        setTimeout(() => {}, 2000);
+        return <Navigate to="/login" />;
+    }
 
-    // const user = userString ? JSON.parse(userString) : null;
-    // console.log("User", user);
-    // if (user.role === "User" && !location.pathname.includes("user-store")) {
-    //     return <Navigate to="/user-store" />;
-    // } else if (user.role === "Farmer") {
-    //     return <Navigate to="/dashboard/farmer" />;
-    // } else if (user.role === "Admin") {
-    //     return <Navigate to="/dashboard/admin" />;
-    // } else {
-    //     return <Navigate to="/login" />;
-    // }
-
-    const token = localStorage.getItem("token");
-
-    if (token) {
+    const user = userString ? JSON.parse(userString) : null;
+    console.log("User", user);
+    if (user.role === "User" && !location.pathname.includes("user-store")) {
+        return <Navigate to="/user-store" />;
+    } else if (
+        user.role === "User" &&
+        location.pathname.includes("user-store")
+    ) {
+        return children;
+    } else if (
+        user.role === "Farmer" &&
+        !location.pathname.includes("dashboard/farmer")
+    ) {
+        return <Navigate to="/dashboard/farmer" />;
+    } else if (
+        user.role === "Farmer" &&
+        location.pathname.includes("dashboard/farmer")
+    ) {
+        return children;
+    } else if (user.role === "Admin" && !location.pathname.includes("admin")) {
+        return <Navigate to="/dashboard/admin" />;
+    } else if (user.role === "Admin" && location.pathname.includes("admin")) {
         return children;
     } else {
         return <Navigate to="/login" />;
     }
+
+    // const token = localStorage.getItem("token");
+
+    // if (token) {
+    //     return children;
+    // } else {
+    //     return <Navigate to="/login" />;
+    // }
 };
 
 export default PrivateRoute;
