6b183aa9e3ff71b39be73be3fbd2fbe8520f9f71
shlok-2003 <shlokprajapati2003@gmail.com>
2024-03-20 01:08:04 +0530

    change config

--- client/src/components/ui/core/dashboard_farmer/index.ts
+++ client/src/components/ui/core/dashboard_farmer/index.ts
@@ -1,4 +1,3 @@
 export { default as LeftComp } from "./left-comp";
 export { default as RightComp } from "./right-comp";
 export { default as AdminHeader } from "./header";
-export { default as BottomChart } from "./bottom-chart";
